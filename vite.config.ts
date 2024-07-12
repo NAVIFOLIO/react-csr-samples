571d62f04ff4ec8b902495f15aa2da978f37f7c1
NAVIFOLIO <programmer@navifolio.jp>
2024-07-12 13:23:01 +0900

    edit some config files to deploy app.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_PAGES
+    ? "react-csr-samples"
+    : "./",
   server: {
     host: true
   },
